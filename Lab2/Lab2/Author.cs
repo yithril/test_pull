bd19aa5c4caee9ea730bda9a13222074a35f6d68
yithril <yithril@gmail.com>
2025-12-06 23:58:42 -0500

    made changes

--- Lab2/Lab2/Author.cs
+++ Lab2/Lab2/Author.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class Author
+    public class Author
     {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Author(int id, string firstName, string lastName)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
--- Lab2/Lab2/Book.cs
+++ Lab2/Lab2/Book.cs
@@ -6,7 +6,27 @@ using System.Threading.Tasks;
 
 namespace Lab2
 {
-    class Book
+    public class Book
     {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public Book(int id, string title, decimal price, int authorId)
+        {
+            Id = id;
+            Title = title;
+            Price = price;
+            AuthorId = authorId;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Title} - {Price:C}";
+        }
     }
 }
